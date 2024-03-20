440835ddc4da423c777856f580b87f6aa95bfaed
Bruno Burin <cburin.bruno@gmail.com>
2024-03-20 10:34:59 -0300

    fix submatrices

--- src/main.py
+++ src/main.py
@@ -11,17 +11,17 @@ im2 = Image.open("../statics/aula-raquel.jpeg")
 p = [(0, 0, 1), (x, 0, 1), (0, y, 1), (x, y, 1)]
 
 # Pontos de destino
-q = [(207, 234, 1), (723, 203, 1), (191, 566, 1), (711, 569, 1)]
+q = [(212, 152, 1), (723, 203, 1), (191, 566, 1), (711, 569, 1)]
 
 a = np.array([[p[k][j] for j in range(3)] for k in range(4)])
-b = [np.diag([q[k][i] for k in range(4)]) for i in range(3)]
+b = [np.diag([-q[k][i] for k in range(4)]) for i in range(3)]
 
 matrix = np.zeros((12, 13))
 
 # Insert submatrices into the final matrix
 matrix[:4, :3] = a
-matrix[4:8, 4:7] = a
-matrix[8:12, 7:10] = a
+matrix[4:8, 3:6] = a
+matrix[8:12, 6:9] = a
 
 matrix[:4, 9:] = b[0]
 matrix[4:8, 9:] = b[1]
